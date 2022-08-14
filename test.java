a32fc74396afe35ee3b8dd858e06bd340e220cd7
Gamotiv <49294700+Gamotiv@users.noreply.github.com>
2022-08-14 20:25:10 +0200

    yes

---
+++ test.java
@@ -0,0 +1,11 @@
+package cost_MAP;
+
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.lang.Math;
+public class test {
+    public static void main(String[] args) {
+        System.out.println( Math.atan2( 3,2 )  );
+    }
+}
